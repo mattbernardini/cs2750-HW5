dbe9f174d1d11659cb0511bdda6f64d4692409bf
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-07 18:50:26 -0600

    changed logic

--- deater/main.c
+++ deater/main.c
@@ -9,13 +9,13 @@ int main (int argc, char* argv[]) {
     char stringArray [argc][256];
     for (int i = 1; i < argc; i++) {
         // Handle if the argument is a integer
-        if (atoi(argv[i]) == 0) {
+        if (atoi(argv[i]) != 0) {
             int tempInt = atoi(argv[i]);
             totalIntegers += tempInt;
         } else {
             // We have characters
         }
     }
-    printf("Total integers: %d\n", &totalIntegers);
+    printf("Total integers: %d\n", totalIntegers);
     return 0;
 }
