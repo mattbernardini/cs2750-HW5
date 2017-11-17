2d98b1ed5e5b10b0454f8c230ed0a715b1099e45
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 12:41:59 -0600

    removed code not needed

--- deater/main.c
+++ deater/main.c
@@ -2,18 +2,14 @@
 
 int main (int argc, char* argv[]) {
     int totalIntegers = 0, arrayCounter = 0;
-    // To store the words we get in command line, initialize to null terminator
-    char stringArray [argc][256];
-    memset(stringArray, '\0', sizeof stringArray);
-
+    // Loop through all arguments
     for (int i = 1; i < argc; i++) {
         // Handle if the argument is a integer
         if (atoi(argv[i]) != 0) {
             int tempInt = atoi(argv[i]);
             totalIntegers += tempInt;
         } else {
-            // We have characters
-            // Copy string into our search array           
+            // We have characters       
             searchForSubstring(arrayCounter++, argv, argv[i]);
         }
     }
