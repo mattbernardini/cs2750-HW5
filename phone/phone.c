27960ae8fa0d3d5c0c5cde23a2e94de1070bd92f
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 12:57:40 -0600

    logic testing

--- phone/phone.c
+++ phone/phone.c
@@ -1,9 +1,12 @@
 #include "phone_fmt.h"
 
 int main (int argc, char* argv[]) {
-    int number[10] = { 0 };
+    int number[9] = { 0 };
+    int counter = 0;
     for (int i = 0; argv[1][i] != '\0'; i++) {
-        printf("%c\n", argv[1][i]);
+        if (atoi(argv[1][i]) != 0) {
+            printf("%c\n", argv[1][i]);
+        }
     }
     return 0;
 }
