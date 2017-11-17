e901c74db0a80175e0716e333f3edb4d0f4b156b
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 12:12:21 -0600

    testing changes

--- deater/func.c
+++ deater/func.c
@@ -2,7 +2,7 @@
 // strstr
 void searchForSubstring (int numStrings, int argc, char** stringArray, char * stringToSearchFor) {
     for (int i = 0; i < numStrings; i++) {
-        if (strstr(stringToSearchFor, stringArray[i]) != NULL) {
+        if (strstr(stringToSearchFor, &stringArray[i]) != NULL) {
             printf("%s is a substring of %s\n", stringToSearchFor, stringArray[i]);
         }
     }
