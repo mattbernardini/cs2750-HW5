34460a015c122b83e504aadb8428551e383c6c9a
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 11:57:04 -0600

    changes to how stuff is being passed

--- deater/func.h
+++ deater/func.h
@@ -2,4 +2,4 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void searchForSubstring (int numStrings, int argc, char [][numStrings] stringArray, char * stringToSearchFor);
+void searchForSubstring (int numStrings, int argc, char ** stringArray, char * stringToSearchFor);
