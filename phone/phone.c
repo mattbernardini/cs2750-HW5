54704143ce27dc8c92fcb81545c908ed8b10e39a
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 12:58:33 -0600

    testing

--- phone/phone.c
+++ phone/phone.c
@@ -4,7 +4,7 @@ int main (int argc, char* argv[]) {
     int number[9] = { 0 };
     int counter = 0;
     for (int i = 0; argv[1][i] != '\0'; i++) {
-        if (atoi(argv[1][i]) != 0) {
+        if (atoi(*argv[1][i]) != 0) {
             printf("%c\n", argv[1][i]);
         }
     }
