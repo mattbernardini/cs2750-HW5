3d2edecb803456bd431b19fe7417e1e6645e059c
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 11:51:49 -0600

    changes

--- deater/func.h
+++ deater/func.h
@@ -2,4 +2,4 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void searchForSubstring (char [M][N] stringArray, char * stringToSearchFor, int numStrings);
+void searchForSubstring (int numStrings, int argc, char [argc][numStrings] stringArray, char * stringToSearchFor);
