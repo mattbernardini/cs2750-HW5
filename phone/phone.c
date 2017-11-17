14a1fe157da4d3799697fc044a60afd3c0970b3f
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 13:00:53 -0600

    logic testing

--- phone/phone.c
+++ phone/phone.c
@@ -4,8 +4,8 @@ int main (int argc, char* argv[]) {
     int number[9] = { 0 };
     int counter = 0;
     for (int i = 0; argv[1][i] != '\0'; i++) {
-        if (isdigit(argv[1][i]) != 0) {
-            printf("%c\n", argv[1][i]);
+        if (isdigit(argv[1][i]) != 0 && counter < 10) {
+            number[counter++] = argv[1][i];
         }
     }
     return 0;
