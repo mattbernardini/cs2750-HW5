6adb4d6b4ef50fe77d9c067ab1fcbadea09c3f43
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 12:40:51 -0600

    change index

--- deater/func.c
+++ deater/func.c
@@ -1,7 +1,7 @@
 #include "func.h"
 // strstr
 void searchForSubstring (int numStrings, char* stringArray[numStrings], char * stringToSearchFor) {
-    for (int i = 0; i < numStrings; i++) {
+    for (int i = 1; i < (numStrings + 1); i++) {
         if (strstr(stringToSearchFor, stringArray[i]) != NULL) {
             printf("%s is a substring of %s\n", stringArray[i], stringToSearchFor);
         }
