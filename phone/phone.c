fc4c1915c5449b391c3b24083d0886c5a43e917b
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 22:02:00 -0600

    tests

--- phone/phone.c
+++ phone/phone.c
@@ -3,7 +3,11 @@
 int main (int argc, char* argv[]) {
     int number[9] = { 0 };
     int counter = 0;
-    parsePhoneNumber(argv, number);
+    for (int i = 0; argv[1][i] != '\0'; i++) {
+        if (isdigit(argv[1][i]) != 0 && counter < 10) {
+            number[counter++] = argv[1][i] - '0';
+        }
+    }
     if (counter == 9) {
         printf("(%d%d%d) %d%d%d-%d%d%d%d", number[0], number[1], number[2], number[3], number[4], number[5], number[6], number[7], number[8], number[9]);
     } else {
