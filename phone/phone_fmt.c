fd0e7042157fe3cf66d6a11b3dd5238659791efc
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 21:55:47 -0600

    messed up c and h

--- phone/phone_fmt.c
+++ phone/phone_fmt.c
@@ -1,2 +1,10 @@
 #include "phone_fmt.h"
-void parsePhoneNumber (char * argv[], int number[]);
+
+void parsePhoneNumber (char * argv[], int number[]) {
+    int counter = 0;
+    for (int i = 0; argv[1][i] != '\0'; i++) {
+        if (isdigit(argv[1][i]) != 0 && counter < 10) {
+            number[counter++] = argv[1][i] - '0';
+        }
+    }
+}
--- phone/phone_fmt.h
+++ phone/phone_fmt.h
@@ -2,11 +2,4 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void parsePhoneNumber (char * argv[], int number[]) {
-    int counter = 0;
-    for (int i = 0; argv[1][i] != '\0'; i++) {
-        if (isdigit(argv[1][i]) != 0 && counter < 10) {
-            number[counter++] = argv[1][i] - '0';
-        }
-    }
-}
+void parsePhoneNumber (char * argv[], int number[]);
