721293ac2f33589338a3320585aa42a1306ee7c6
Matt Bernardini <matt.bernardini@gmail.com>
2017-11-17 22:04:38 -0600

    changed return

--- phone/phone.c
+++ phone/phone.c
@@ -2,8 +2,7 @@
 
 int main (int argc, char* argv[]) {
     int number[9] = { 0 };
-    int counter = 0;
-    parsePhoneNumber(argv, number);
+    int counter = parsePhoneNumber(argv, number);
     if (counter == 9) {
         printf("(%d%d%d) %d%d%d-%d%d%d%d", number[0], number[1], number[2], number[3], number[4], number[5], number[6], number[7], number[8], number[9]);
     } else {
--- phone/phone_fmt.h
+++ phone/phone_fmt.h
@@ -2,4 +2,4 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void parsePhoneNumber (char * argv[], int number[]);
+int parsePhoneNumber (char * argv[], int number[]);
